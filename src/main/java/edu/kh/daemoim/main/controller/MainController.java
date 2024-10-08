822172ba100816e2d79d93cc04c0f2902288aad0
Glenlook <shawn9701@naver.com>
2024-10-08 17:53:46 +0900

    유준선 커밋

--- src/main/java/edu/kh/daemoim/main/controller/MainController.java
+++ src/main/java/edu/kh/daemoim/main/controller/MainController.java
@@ -4,16 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 
 @Controller
-@Slf4j
 @RequiredArgsConstructor
-@RequestMapping
 public class MainController {
-
 	
-
+	@RequestMapping("/")
+	public String mainPage() {
+		return "common/main";
+	}
 	
 }
