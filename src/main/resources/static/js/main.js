17e43c2b472b500711fde8e741136b9b7cd84371
yukpoyum <anokata@naver.com>
2024-10-18 15:50:49 +0900

    20241018_메인페이지, 검색, 카테고리 페이지 html 업로드

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,30 +1,41 @@
-// 팝업창
-window.onload = function() {
-  document.getElementById("popup").classList.add("active");
-  document.getElementById("popupOverlay").classList.add("active");
-}
+document.addEventListener("DOMContentLoaded", function() {
+  // 팝업창
+  const popup = document.getElementById("popup");
+  const popupOverlay = document.getElementById("popupOverlay");
+  const closePopupBtn = document.getElementById("closePopup");
 
-// 팝업 닫기 버튼 클릭 시 팝업 닫기
-document.getElementById("closePopup").onclick = function() {
-  document.getElementById("popup").classList.remove("active");
-  document.getElementById("popupOverlay").classList.remove("active");
-}
+  if (popup && popupOverlay) {
+    popup.classList.add("active");
+    popupOverlay.classList.add("active");
+  
+    // 팝업 닫기 버튼 클릭 시 팝업 닫기
+    closePopupBtn.onclick = function() {
+      popup.classList.remove("active");
+      popupOverlay.classList.remove("active");
+    }
+  }
 
-// -------------------------------------------------------------------------
+  // 로그인 버튼 클릭 시
+  const loginBtn = document.getElementById("login-btn");
+  if (loginBtn) {
+    loginBtn.addEventListener("click", function() {
+      const loginWrapper = document.querySelector(".login-wrapper");
 
-// 로그인 버튼 클릭 시
-document.getElementById("login-btn").addEventListener("click", function() {
-  const loginBtn = document.querySelector(".login");
+      // 로그인 버튼 -> 알림과 마이페이지 버튼으로 변경
+      loginWrapper.innerHTML = `
+        <button id="alert-btn">알림</button>
+        <button id="mypage-btn">마이페이지</button>
+      `;
 
-  // 로그인 버튼 -> 알림과 마이페이지 버튼
-  loginBtn.innerHTML = `
-    <button id="alert-btn">알림</button>
-    <button id="mypage-btn">마이페이지</button>
-  `;
+      // 알림 버튼 클릭 시
+      const alertBtn = document.getElementById("alert-btn");
+      alertBtn.addEventListener("click", function() {
+      });
 
-  document.getElementById("alert-btn").addEventListener("click", function() {
-  });
-
-  document.getElementById("mypage-btn").addEventListener("click", function() {
-  });
+      // 마이페이지 버튼 클릭 시
+      const mypageBtn = document.getElementById("mypage-btn");
+      mypageBtn.addEventListener("click", function() {
+      });
+    });
+  }
 });
